f99fe32839fc3d4a19472c48b2d56a7172386fb8
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-12-08 20:53:34 +0530

    mm

--- Client/tuneunity/src/components/Player.jsx
+++ Client/tuneunity/src/components/Player.jsx
@@ -63,7 +63,7 @@ const Player = () => {
             className="w-full max-w-4xl aspect-video rounded-lg shadow-lg overflow-hidden"
           >
             <iframe
-              src={`https://www.youtube.com/embed/${video.id.videoId}?rel=0&enablejsapi=1&controls=1`}
+              src={`https://www.youtube.com/embed/${video.id.videoId}?rel=0&enablejsapi=1&controls=1&autoplay=1`}
               title={video.snippet.title}
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
