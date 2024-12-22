12cdb4817ead67942bfdc245a3b9770d20109d41
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-12-22 20:00:48 +0530

    component clr changed!

--- Client/tuneunity/src/components/MyText.jsx
+++ Client/tuneunity/src/components/MyText.jsx
@@ -14,8 +14,8 @@ const MyText = (props) => {
       }
   return (
     <div className='w-full h-auto flex text-white flex flex-col items-end'>
-        <div className='w-[60%] bg-[#252323] pl-3 pr-3 pt-2 pb-2 m-2 rounded-md'>
-          <div className='w-full flex justify-start font-light text-green-500'>Me</div>
+        <div className='w-[60%]  bg-[#125838] pl-3 pr-3 pt-2 pb-2 m-2 rounded-md'>
+          <div className='w-full flex justify-start text-white font-bold'>Me</div>
            <p className=''>{props.text}</p>
         <p className='w-full flex justify-end'>{getFormattedTime()}</p>
         </div>
