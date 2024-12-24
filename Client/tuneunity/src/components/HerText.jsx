b8a61d3cfd25d7c78204ffd55ce118979106d879
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-12-24 16:54:59 +0530

    fix

--- Client/tuneunity/src/components/HerText.jsx
+++ Client/tuneunity/src/components/HerText.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import Chatbg from '../assets/chatbg.png';
 
 const HerText = (props) => {
-  const [glow,setglow]=useState(false);
+  const [glow,setglow]=useState(true);
   setInterval(()=>{
     setglow(!glow);
-  },1000);
+  },5000);
   return (
     <>
       {props.song ? (
@@ -24,10 +24,10 @@ const HerText = (props) => {
             <p className="">{props.text}</p>
             <p className="w-full flex justify-end">{props.time}</p>
           </div>
-        </div>
+        </div> 
       ) : (
         <div className="w-full h-auto flex text-white flex-col items-start">
-          <div className="w-[60%] bg-[#125838] pl-3 pr-3 pt-2 pb-2 m-2 rounded-md">
+          <div className="w-[60%] bg-[#252323] pl-3 pr-3 pt-2 pb-2 m-2 rounded-md">
             <div className="w-full flex justify-start text-white font-bold">{props.name}</div>
             <p className="">{props.text}</p>
             <p className="w-full flex justify-end">{props.time}</p>
